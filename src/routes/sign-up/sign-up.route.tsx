7c8a9512ad47f6c22a0df5030b4d8cfee77d4b05
olashina201 <olashina201@gmail.com>
2022-11-18 12:08:37 +0100

    updte codebase

--- src/routes/sign-up/sign-up.route.tsx
+++ src/routes/sign-up/sign-up.route.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { ChangeEvent, ChangeEventHandler } from "react";
 import {
   FormContainer,
   FormTitle,
@@ -11,30 +11,76 @@ import {
 import signupImage from "../../assets/signup.png";
 
 export const SignUp = () => {
+  const [formValues, setFormValues] = React.useState({
+    name: "",
+  });
+
+  const handleChange: ChangeEventHandler = (
+    e: ChangeEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    console.log(formValues);
+  };
+
   return (
     <SignUpContainer>
       <SignUpContent>
         <FormContainer>
           <form>
             <FormTitle>Create an account</FormTitle>
-            <input type="text" name="" placeholder="Username" />
-            <input type="email" name="" placeholder="Email Address" />
-            <input type="email" name="" placeholder="Email Address" />
-            <input type="email" name="" placeholder="Email Address" />
-            <input type="email" name="" placeholder="Email Address" />
-            <input type="password" name="" placeholder="Create Password" />
-            <input type="password" name="" placeholder="Confirm Password" />
-            <SignUpButton type="submit" name="" value="Sign Up" />
+            <input
+              type="text"
+              name="fullname"
+              placeholder="Username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name=""
+              placeholder="Email Address"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name=""
+              placeholder="Email Address"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name=""
+              placeholder="Email Address"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name=""
+              placeholder="Create Password"
+              onChange={handleChange}
+            />
+            <SignUpButton
+              type="submit"
+              name=""
+              value="Sign Up"
+              onClick={handleSubmit}
+            />
             <p>
               Already have an account ?<a href="/login">Sign in.</a>
             </p>
           </form>
         </FormContainer>
         <ImageBox>
-          <img
-            src={signupImage}
-            alt=""
-          />
+          <img src={signupImage} alt="" />
         </ImageBox>
       </SignUpContent>
     </SignUpContainer>
